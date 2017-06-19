363cdb913b6985a1bba08e28a1b2173aa81e2cf5
dingpl716 <dingpl716@gmail.com>
2017-06-19 16:59:30 -0700

    Adding LRUCache and MinStack to design package.

---
+++ LeetCode/LeetCodeByCategory/src/design/MinStack.java
@@ -0,0 +1,55 @@
+package design;
+import java.util.Stack;
+
+//	Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+//	
+//	push(x) -- Push element x onto stack.
+//	pop() -- Removes the element on top of the stack.
+//	top() -- Get the top element.
+//	getMin() -- Retrieve the minimum element in the stack.
+
+public class MinStack {
+
+	private Stack<Integer> dataStack = new Stack<Integer>();
+	private Stack<Integer> minStack = new Stack<Integer>();
+	
+    public void push(int x) {
+    	if (dataStack.isEmpty()){
+    		dataStack.push(x);
+    		minStack.push(x);
+    	}else{
+    		dataStack.push(x);
+    		if (x <= minStack.peek()){
+    			minStack.push(x);
+    		}
+    	}
+    }
+
+    public void pop() {
+    	if (dataStack.isEmpty()){
+    		return;
+    	}
+    	
+    	int top = dataStack.pop();
+    	if (top == minStack.peek()){
+    		minStack.pop();
+    	}
+    	
+    	if (dataStack.isEmpty()){
+    		minStack.clear();
+    	}
+    }
+
+    public int top() {
+        return dataStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+    
+	public static void main(String[] args) {
+		
+	}
+
+}
